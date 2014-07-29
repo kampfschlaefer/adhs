8302908992291de040f4c13cd6bb37c83d067a66
Arnold Krille <arnold@arnoldarts.de>
2014-07-29 20:44:01 +0200

    start with the server-stub and a first failing test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from adhs.client import AdhsClient
+from adhs.server import AdhsServer
 
 class TestClient(object):
     def test_reach_zero_servers(self):
         '''As no server is started, asking the number of servers should be 0'''
         c = AdhsClient()
         assert len(c.active_servers()) == 0
+
+    def test_start_one_server(self):
+        AdhsServer()
+
+        c = AdhsClient()
+        assert len(c.active_servers()) == 1
